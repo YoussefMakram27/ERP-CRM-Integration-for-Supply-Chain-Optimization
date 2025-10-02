e0800e14a24e3b7f3a361b6d52bec1dff2eec9ab
YoussefMakram27 <yousefmakram27@gmail.com>
2025-10-02 11:37:49 +0200

    Finished Silver Layer

---
+++ Scripts/Silver_Layer/InsertingCRMDataInSilver.sql
@@ -0,0 +1,265 @@
+/********************************************************************************************
+ Script Name   : InsertingCRMDataInSilver.sql
+ Description   : This script cleanse the CRM data and then insert it into silver layer tables.
+
+ Author        : Youssef M.Makram M.Osman
+ Created Date  : 2025-10-01
+
+ Target DB     : ERP_CRM
+*********************************************************************************************/
+
+use ERP_CRM;
+
+---------------------------------------------------
+-- Load CRM Customers
+---------------------------------------------------
+
+print '=== Starting load for CRM Customers...';
+
+merge Silver.crm_customers as target
+using (
+	select 
+		TRIM(customer_id) AS customer_id,
+
+		nullif(trim(customer_name), '') as customer_name,
+
+		case 
+			when location in ('N/A', 'Unknown', '') then NULL
+			else trim(location)
+		end as location,
+
+		trim(industry_type) AS industry_type,
+
+		trim(account_manager) as account_manager
+
+	from (
+		select *,
+			   ROW_NUMBER() over(
+					partition by customer_id
+					order by customer_id
+			   ) flag
+		from Bronze.crm_customers
+	) f 
+	where flag = 1 and customer_id is not null
+) as source
+	on target.customer_id = source.customer_id
+
+when matched then
+	update set
+		-- you don't need to update customer_id when matched
+		-- If it changes, it usually means a new record instead of an update.
+		target.customer_name = source.customer_name,
+		target.location = source.location,
+		target.industry_type = source.industry_type,
+		target.account_manager = source.account_manager
+
+when not matched then
+	insert (customer_id, customer_name, location, industry_type, account_manager, source_file,
+			load_datetime)
+	values (source.customer_id, source.customer_name, source.location, source.industry_type,
+			source.account_manager, 'Bronze.crm_customers.csv', sysdatetime());
+	
+PRINT '>>> Finished load for CRM Customers.';
+
+go
+
+---------------------------------------------------
+-- Load CRM Returns
+---------------------------------------------------
+print '=== Starting load for CRM Returns...';
+
+merge Silver.crm_returns as target
+using (
+	select 
+		  trim(return_id) as return_id,
+		  nullif(trim(order_id), '') as order_id,
+		  nullif(trim(sku), '') as sku,
+
+		  case 
+            when isnumeric(returns_quantity) = 1 
+                 and TRY_CONVERT(int, returns_quantity) >= 0
+                then TRY_CONVERT(int, returns_quantity)
+            else NULL
+		  end as returns_quantity,
+
+		  case 
+            when return_reason IN ('N/A','Unknown','') then NULL
+		    else trim(return_reason)
+		  end as return_reason,
+
+		  case 
+				when TRY_CONVERT(datetime2, return_date, 23) is not NULL
+					then CONVERT(datetime2, return_date, 23)
+				when TRY_CONVERT(datetime2, return_date, 105) is not NULL
+					then CONVERT(datetime2, return_date, 105)
+				when TRY_CONVERT(datetime2, return_date, 101) is not NULL
+					then CONVERT(datetime2, return_date, 101)
+				else NULL
+		  end as return_date
+	from (
+        select *,
+               ROW_NUMBER() over (
+                   partition by return_id
+                   order by return_id
+               ) flag
+        from Bronze.crm_returns
+    ) f
+    where flag = 1
+      and return_id is not NULL
+) as source
+	on target.return_id = source.return_id
+
+when matched then
+	update set
+		target.order_id = source.order_id,
+		target.sku = source.sku,
+		target.returns_quantity = source.returns_quantity,
+		target.return_reason = source.return_reason,
+		target.return_date = source.return_date
+
+when not matched then
+	insert (return_id, order_id, sku, returns_quantity, return_reason, return_date, 
+			source_file, load_datetime)
+	values (source.return_id, source.order_id, source.sku, source.returns_quantity,
+			source.return_reason, source.return_date, 'Bronze.crm_returns.csv', sysdatetime());
+		
+print '>>> Finished load for CRM Returns.';
+
+go
+
+---------------------------------------------------
+-- Load CRM Sales Leads
+---------------------------------------------------
+print '=== Starting UPSERT into Silver.crm_sales_leads...';
+
+merge Silver.crm_sales_leads as target
+using (
+    select 
+        trim(lead_id) AS lead_id,
+
+        nullif(trim(customer_id), '') AS customer_id,
+
+        case 
+            when product_interest in ('', 'N/A', 'Unknown') then NULL
+            else trim(product_interest)
+        end as product_interest,
+
+        case 
+            when ISNUMERIC(expected_order_quantity) = 1
+                 and TRY_CONVERT(int, expected_order_quantity) > 0
+                then TRY_CONVERT(int, expected_order_quantity)
+            else NULL
+        end as expected_order_quantity,
+
+        case 
+            when TRY_CONVERT(datetime2, expected_order_date, 23) is not NULL
+                 and TRY_CONVERT(datetime2, expected_order_date, 23) between '2000-01-01' and DATEADD(YEAR, 2, GETDATE())
+                then CONVERT(datetime2, expected_order_date, 23)
+            when TRY_CONVERT(datetime2, expected_order_date, 105) is not NULL
+                 and TRY_CONVERT(datetime2, expected_order_date, 105) between '2000-01-01' and DATEADD(YEAR, 2, GETDATE())
+                then CONVERT(datetime2, expected_order_date, 105)
+            when TRY_CONVERT(datetime2, expected_order_date, 101) is not NULL
+                 and TRY_CONVERT(datetime2, expected_order_date, 101) between '2000-01-01' and DATEADD(YEAR, 2, GETDATE())
+                then CONVERT(datetime2, expected_order_date, 101)
+            else NULL
+        end as expected_order_date
+
+    from (
+        select *,
+               ROW_NUMBER() over (
+                   partition by lead_id
+                   order by lead_id
+               ) flag
+        from Bronze.crm_sales_leads
+    ) f
+    where flag = 1
+      and lead_id is not NULL
+) as source
+on target.lead_id = source.lead_id
+
+when matched then
+    update set
+        target.customer_id = source.customer_id,
+        target.product_interest = source.product_interest,
+        target.expected_order_quantity = source.expected_order_quantity,
+        target.expected_order_date = source.expected_order_date
+
+when not matched then
+    insert (lead_id, customer_id, product_interest, expected_order_quantity, expected_order_date, 
+            source_file, load_datetime)
+    values (source.lead_id, source.customer_id, source.product_interest, source.expected_order_quantity,
+            source.expected_order_date, 'Bronze.crm_sales_leads.csv', sysdatetime());
+
+PRINT '>>> Finished UPSERT into Silver.crm_sales_leads.';
+
+go
+
+---------------------------------------------------
+-- Load CRM Sales Orders
+---------------------------------------------------
+print '=== Starting UPSERT into Silver.crm_sales_orders...';
+
+merge Silver.crm_sales_orders as target
+using (
+    select 
+        trim(order_id) AS order_id,
+
+        nullif(trim(customer_id), '') AS customer_id,
+
+        nullif(trim(sku), '') AS sku,
+
+        case 
+            when ISNUMERIC(REPLACE(order_quantity, '-', '')) = 1 
+                 and TRY_CONVERT(int, REPLACE(order_quantity, '-', '')) >= 0
+                then TRY_CONVERT(int, REPLACE(order_quantity, '-', ''))
+            else NULL
+        end as order_quantity,
+
+        case 
+            when TRY_CONVERT(datetime2, order_date, 23) is not NULL
+                 and TRY_CONVERT(datetime2, order_date, 23) between '2000-01-01' and DATEADD(YEAR, 1, GETDATE())
+                then CONVERT(datetime2, order_date, 23)
+            when TRY_CONVERT(datetime2, order_date, 105) is not NULL
+                 and TRY_CONVERT(datetime2, order_date, 105) between '2000-01-01' and DATEADD(YEAR, 1, GETDATE())
+                then CONVERT(datetime2, order_date, 105)
+            when TRY_CONVERT(datetime2, order_date, 101) is not NULL
+                 and TRY_CONVERT(datetime2, order_date, 101) between '2000-01-01' and DATEADD(YEAR, 1, GETDATE())
+                then CONVERT(datetime2, order_date, 101)
+            else NULL
+        end as order_date,
+
+        case 
+            when trim(order_status) in ('', 'Unknown', 'N/A') then NULL
+            else trim(order_status)
+        end as order_status
+
+    from (
+        select *,
+               ROW_NUMBER() over (
+                   partition by order_id
+                   order by order_id
+               ) flag
+        from Bronze.crm_sales_orders
+    ) f
+    where flag = 1
+      and order_id is not NULL
+) as source
+on target.order_id = source.order_id
+
+when matched then
+    update set
+        target.customer_id = source.customer_id,
+        target.sku = source.sku,
+        target.order_quantity = source.order_quantity,
+        target.order_date = source.order_date,
+        target.order_status = source.order_status
+
+when not matched then
+    insert (order_id, customer_id, sku, order_quantity, order_date, order_status, 
+            source_file, load_datetime)
+    values (source.order_id, source.customer_id, source.sku, source.order_quantity, 
+            source.order_date, source.order_status, 'Bronze.crm_sales_orders.csv', sysdatetime());
+
+PRINT '>>> Finished UPSERT into Silver.crm_sales_orders.';
+
+go
---
+++ Scripts/Silver_Layer/InsertingERPDataInSilver.sql
@@ -0,0 +1,260 @@
+/********************************************************************************************
+ Script Name   : InsertingERPDataInSilver.sql
+ Description   : Cleanse the ERP data and then merge it into silver layer tables.
+
+ Author        : Youssef M.Makram M.Osman
+ Created Date  : 2025-10-01
+ 
+ Target DB     : ERP_CRM
+*********************************************************************************************/
+
+use ERP_CRM;
+
+-- ============================
+-- erp_inventory
+-- ============================
+print 'starting erp_inventory cleanse and load...';
+
+merge Silver.erp_inventory as target
+using (
+    select sku,
+           warehouse_id,
+           case 
+				when isnumeric(replace(current_stock,'-',''))=1 
+				then cast(replace(current_stock,'-','') as int) 
+				else 0 
+		   end as current_stock,
+
+           case 
+				when isnumeric(replace(safety_stock,'-',''))=1 
+				then cast(replace(safety_stock,'-','') as int) 
+				else 0 
+		   end as safety_stock,
+
+           case 
+				when try_convert(datetime2, inventory_last_updated, 23) is not null
+                 and convert(datetime2, inventory_last_updated, 23) <= sysdatetime()
+                then convert(datetime2, inventory_last_updated, 23)
+                when try_convert(datetime2, inventory_last_updated, 103) is not null
+                 and convert(datetime2, inventory_last_updated, 103) <= sysdatetime()
+                then convert(datetime2, inventory_last_updated, 103)
+                when try_convert(datetime2, inventory_last_updated, 101) is not null
+                 and convert(datetime2, inventory_last_updated, 101) <= sysdatetime()
+                then convert(datetime2, inventory_last_updated, 101)
+                else null 
+		   end as inventory_last_updated
+
+    from Bronze.erp_inventory
+    where sku is not null and warehouse_id is not null
+) as source
+	on target.sku = source.sku and target.warehouse_id = source.warehouse_id
+when matched then
+    update set 
+			target.current_stock = source.current_stock,
+            target.safety_stock = source.safety_stock,
+            target.inventory_last_updated = source.inventory_last_updated
+
+when not matched then
+    insert (sku, warehouse_id, current_stock, safety_stock, inventory_last_updated,
+			source_file, load_datetime)
+    values (source.sku, source.warehouse_id, source.current_stock, source.safety_stock,
+			source.inventory_last_updated, 'Bronze.erp_inventory.csv', sysdatetime());
+
+print 'finished erp_inventory load.';
+
+go
+
+-- ============================
+-- erp_inventory_transactions
+-- ============================
+print 'starting erp_inventory_transactions cleanse and load...';
+
+merge Silver.erp_inventory_transactions as target
+using (
+    select transaction_id,
+           sku,
+           warehouse_id,
+           trim(transaction_type) as transaction_type,
+
+           case 
+				when change_quantity = 0 
+				then null 
+				else change_quantity 
+		   end as change_quantity,
+
+           case 
+				when try_convert(datetime2, replace(transaction_timestamp,'T',' '), 23) is not null
+                then convert(datetime2, replace(transaction_timestamp,'T',' '), 23)
+                when try_convert(datetime2, replace(transaction_timestamp,'T',' '), 103) is not null
+                then convert(datetime2, replace(transaction_timestamp,'T',' '), 103)
+                when try_convert(datetime2, replace(transaction_timestamp,'T',' '), 101) is not null
+                then convert(datetime2, replace(transaction_timestamp,'T',' '), 101)
+                else null 
+		   end as transaction_timestamp
+
+    from Bronze.erp_inventory_transactions
+    where transaction_id is not null
+) as source
+	on target.transaction_id = source.transaction_id
+
+when matched then
+    update set 
+			target.sku = source.sku,
+            target.warehouse_id = source.warehouse_id,
+            target.transaction_type = source.transaction_type,
+            target.change_quantity = source.change_quantity,
+            target.transaction_timestamp = source.transaction_timestamp
+
+when not matched then
+    insert (transaction_id, sku, warehouse_id, transaction_type, change_quantity,
+			transaction_timestamp, source_file, load_datetime)
+    values (source.transaction_id, source.sku, source.warehouse_id, source.transaction_type,
+			source.change_quantity, source.transaction_timestamp, 
+			'Bronze.erp_inventory_transactions.csv', sysdatetime());
+
+print 'finished erp_inventory_transactions load.';
+go
+
+
+-- ============================
+-- erp_products
+-- ============================
+print 'starting erp_products cleanse and load...';
+
+merge Silver.erp_products as target
+using (
+    select sku,
+           trim(replace(product_name,'"','')) as product_name,
+           trim(product_category) as product_category,
+
+           case 
+				when try_cast(product_price as decimal(10,2)) is not null 
+				and cast(product_price as decimal(10,2)) > 0
+                then cast(product_price as decimal(10,2)) else null 
+		   end as product_price,
+
+           supplier_id
+
+    from Bronze.erp_products
+    where sku is not null
+) as source
+	on target.sku = source.sku
+when matched then
+    update set 
+			target.product_name = source.product_name,
+            target.product_category = source.product_category,
+            target.product_price = source.product_price,
+            target.supplier_id = source.supplier_id
+
+when not matched then
+    insert (sku, product_name, product_category, product_price, supplier_id,
+			source_file, load_datetime)
+    values (source.sku, source.product_name, source.product_category, source.product_price,
+			source.supplier_id, 'Bronze.erp_products.csv', sysdatetime());
+
+print 'finished erp_products load.';
+go
+
+
+-- ============================
+-- erp_purchase_orders
+-- ============================
+print 'starting erp_purchase_orders cleanse and load...';
+
+merge Silver.erp_purchase_orders as target
+using (
+    select purchase_order_id,
+           supplier_id,
+           sku,
+           case 
+				when isnumeric(ordered_quantity)=1 and cast(ordered_quantity as int) > 0
+                then cast(ordered_quantity as int) 
+				else null 
+		   end as ordered_quantity,
+
+           case 
+				when order_date_cleaned <= expected_delivery_date_cleaned
+                then order_date_cleaned 
+				else expected_delivery_date_cleaned 
+		   end as order_date,
+
+           case 
+				when expected_delivery_date_cleaned >= order_date_cleaned
+                then expected_delivery_date_cleaned 
+				else order_date_cleaned 
+		   end as expected_delivery_date,
+
+           trim(delivery_status) as delivery_status
+
+    from (
+        select purchase_order_id,
+               supplier_id,
+               sku,
+               ordered_quantity,
+               case when try_convert(datetime2, order_date, 23) is not null then convert(datetime2, order_date, 23)
+                    when try_convert(datetime2, order_date, 103) is not null then convert(datetime2, order_date, 103)
+                    when try_convert(datetime2, order_date, 101) is not null then convert(datetime2, order_date, 101)
+                    else null end as order_date_cleaned,
+               case when try_convert(datetime2, expected_delivery_date, 23) is not null then convert(datetime2, expected_delivery_date, 23)
+                    when try_convert(datetime2, expected_delivery_date, 103) is not null then convert(datetime2, expected_delivery_date, 103)
+                    when try_convert(datetime2, expected_delivery_date, 101) is not null then convert(datetime2, expected_delivery_date, 101)
+                    else null end as expected_delivery_date_cleaned,
+
+               trim(delivery_status) as delivery_status
+        from Bronze.erp_purchase_orders
+    ) f
+    where purchase_order_id is not null
+) as source
+	on target.purchase_order_id = source.purchase_order_id
+when matched then
+    update set 
+			target.supplier_id = source.supplier_id,
+            target.sku = source.sku,
+            target.ordered_quantity = source.ordered_quantity,
+            target.order_date = source.order_date,
+            target.expected_delivery_date = source.expected_delivery_date,
+            target.delivery_status = source.delivery_status
+
+when not matched then
+    insert (purchase_order_id, supplier_id, sku, ordered_quantity, order_date, 
+			expected_delivery_date, delivery_status, source_file, load_datetime)
+    values (source.purchase_order_id, source.supplier_id, source.sku, source.ordered_quantity,
+			source.order_date, source.expected_delivery_date, source.delivery_status,
+			'Bronze.erp_purchase_orders.csv', sysdatetime());
+
+print 'finished erp_purchase_orders load.';
+go
+
+
+-- ============================
+-- erp_suppliers
+-- ============================
+print 'starting erp_suppliers cleanse and load...';
+
+merge Silver.erp_suppliers as target
+using (
+    select supplier_id,
+           trim(supplier_name) as supplier_name,
+           case when try_cast(lead_time_days as int) is not null and cast(lead_time_days as int) >= 0
+                then cast(lead_time_days as int) else null end as lead_time_days,
+
+           case when try_cast(reliability_score as int) between 0 and 100
+                then cast(reliability_score as int) else null end as reliability_score
+
+    from Bronze.erp_suppliers
+    where supplier_id is not null
+) as source
+	on target.supplier_id = source.supplier_id
+when matched then
+    update set 
+			target.supplier_name = source.supplier_name,
+            target.lead_time_days = source.lead_time_days,
+            target.reliability_score = source.reliability_score
+
+when not matched then
+    insert (supplier_id, supplier_name, lead_time_days, reliability_score, source_file, load_datetime)
+    values (source.supplier_id, source.supplier_name, source.lead_time_days,
+	        source.reliability_score, 'Bronze.erp_suppliers.csv', sysdatetime());
+
+print 'finished erp_suppliers load.';
+go
